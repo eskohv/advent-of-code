bb23558b6fcea135d994f6416496c1bc3c04b17e
Eetu Kohvakka <eskohv@utu.fi>
2021-12-15 21:19:33 +0200

    Initial commit with instructions in Main.java

---
+++ src/com/company/Day3.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Day3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * Empty main function to retain code integrity
+         *
+         * Create new file:
+         * Under com.company Command + N, New Java Class, name it "DayX"
+         * Or File > New file > Java Class > Name
+         *
+         * Run code:
+         * Right click editor > 'Run Day1.main()'
+         */
+    }
+}
